57a66961c528c93bfd3bfb990477d04caf491a3a
Ruslan Vaitukevich <rusikruslanchik@mail.ru>
2024-03-07 11:45:58 +0300

    Major changes

--- QTLabs/Lab_03_SignalsAndSlots/Translator/mainwindow.cpp
+++ QTLabs/Lab_03_SignalsAndSlots/Translator/mainwindow.cpp
@@ -10,6 +10,8 @@ MainWindow::MainWindow(QWidget *parent)
 
     connect(ui->textEdit, SIGNAL(textChanged()), this, SLOT(slotConvertTextEdit()));
     connect(ui->textEdit_2, SIGNAL(textChanged()), this, SLOT(slotConvertTextEdit_2()));
+    connect(ui->comboBox, SIGNAL(activated(int)), this, SLOT(slotConvertTextEdit_2()));
+    connect(ui->comboBox_2, SIGNAL(activated(int)), this, SLOT(slotConvertTextEdit()));
 }
 
 MainWindow::~MainWindow()
@@ -19,18 +21,69 @@ MainWindow::~MainWindow()
 
 void MainWindow::slotConvertTextEdit() {
     ui->textEdit_2->blockSignals(true);
+    QString input = ui->comboBox->currentText();
+    QString output = ui->comboBox_2->currentText();
+    int inputNumeralSystem = 0;
+    int outputNumeralSystem = 0;
+
+    if (input == "Binary") {
+        inputNumeralSystem = 2;
+    } else if (input == "Octal") {
+        inputNumeralSystem = 8;
+    } else if (input == "Decimal") {
+        inputNumeralSystem = 10;
+    } else if (input == "Hexadecimal") {
+        inputNumeralSystem = 16;
+    }
+
+    if (output == "Binary") {
+        outputNumeralSystem = 2;
+    } else if (output == "Octal") {
+        outputNumeralSystem = 8;
+    } else if (output == "Decimal") {
+        outputNumeralSystem = 10;
+    } else if (output == "Hexadecimal") {
+        outputNumeralSystem = 16;
+    }
+
     bool ok;
-    qint64 number = (ui->textEdit->toPlainText()).toLongLong(&ok, 10);
-    QString result = QString::number(number, 2);
-    ui->textEdit_2->setText(result.toUpper());
+    qint64 number = (ui->textEdit->toPlainText()).toLongLong(&ok, inputNumeralSystem);
+    QString result = QString::number(number, outputNumeralSystem);
+    ui->textEdit_2->setText(result);
     ui->textEdit_2->blockSignals(false);
 }
 
 void MainWindow::slotConvertTextEdit_2() {
     ui->textEdit->blockSignals(true);
+
+    QString input = ui->comboBox_2->currentText();
+    QString output = ui->comboBox->currentText();
+    int inputNumeralSystem = 0;
+    int outputNumeralSystem = 0;
+
+    if (input == "Binary") {
+        inputNumeralSystem = 2;
+    } else if (input == "Octal") {
+        inputNumeralSystem = 8;
+    } else if (input == "Decimal") {
+        inputNumeralSystem = 10;
+    } else if (input == "Hexadecimal") {
+        inputNumeralSystem = 16;
+    }
+
+    if (output == "Binary") {
+        outputNumeralSystem = 2;
+    } else if (output == "Octal") {
+        outputNumeralSystem = 8;
+    } else if (output == "Decimal") {
+        outputNumeralSystem = 10;
+    } else if (output == "Hexadecimal") {
+        outputNumeralSystem = 16;
+    }
+
     bool ok;
-    qint64 number = (ui->textEdit_2->toPlainText()).toLongLong(&ok, 2);
-    QString result = QString::number(number, 10);
+    qint64 number = (ui->textEdit_2->toPlainText()).toLongLong(&ok, inputNumeralSystem);
+    QString result = QString::number(number, outputNumeralSystem);
     ui->textEdit->setText(result);
     ui->textEdit->blockSignals(false);
 }
